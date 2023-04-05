9fb86a579a6caf024af46222ea0b633a501f1dd7
QUEEN <vishwakumar.petit@gmail.com>
2023-04-05 18:37:23 +0530

    cleanup

--- locator.py
+++ locator.py
@@ -1,10 +1,5 @@
 import geocoder
-# import requests
+
 def findlocation():
     location = geocoder.ip('me')
     return [location.lat, location.lng]
-# url = "https://nominatim.openstreetmap.org/reverse?format=json&lat={0}&lon={1}".format(location.lat, location.lng)
-# response = requests.get(url).json()
-#         # extract the address and display it on the console
-# address = response["display_name"]
-# print(address)
